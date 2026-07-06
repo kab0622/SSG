23c1d30cef9db9bf3f17929491d0e84bd750f324
kab0622 <kab0622@gmail.com>
2026-07-06 17:11:13 -0500

    fix links

--- src/extract_title.py
+++ src/extract_title.py
@@ -2,7 +2,7 @@ def extract_title(markdown):
     lines = markdown.split("\n")
     for line in lines:
         if line[0] == "#" and line[1] != "#":
-            title = markdown.lstrip("#").lstrip(" ")
+            title = line.lstrip("#").lstrip(" ")
             return title
         continue
     raise Exception("No title in markdown")
